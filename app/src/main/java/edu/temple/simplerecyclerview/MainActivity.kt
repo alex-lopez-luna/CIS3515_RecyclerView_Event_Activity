f1c671068daf0981e266edd1847bbc57b434ae78
alex-lopez-luna <tur54397@temple.edu>
2024-09-26 14:54:40 -0400

    Followed instructions done by professor

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,9 +19,15 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = GridLayoutManager(this, 4)
 
         // TODO (Step 2: Define lambda to modify displayTextView size)
+     /*   val callback = {
+            number: Int ->
+            displayTextView.textSize = number.toFloat()
+        }
+        if going to use this then do NumberDisplayAdapter(numbers, callback) instead of what it is right now
+      */
 
         // Todo (Step 3: Pass lambda to adapter)
-        recyclerView.adapter = NumberDisplayAdapter(numbers)
+        recyclerView.adapter = NumberDisplayAdapter(numbers) {number: Int -> displayTextView.textSize = number.toFloat() }
 
 
     }
